f8d60ddb913790021a19640d985f55de15c72fe1
Teo <teo.bostiog@yahoo.com>
2025-02-19 20:45:17 +0200

    appwrite still doesnt work

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,32 +1,7 @@
 import { ID, Query } from "node-appwrite";
-import { users } from "../appwrite.config";
 import { parseStringify } from "../utils";
+import { users } from "../appwrite.config";
 
-export const createUser = async (user: CreateUserParams) => {
-  try {
-    // Create new user -> https://appwrite.io/docs/references/1.5.x/server-nodejs/users#create
-    const newuser = await users.create(
-      ID.unique(),
-      user.email,
-      user.phone,
-      undefined,
-      user.name
-    );
-
-    return parseStringify(newuser);
-  } catch (error: any) {
-    // Check existing user
-    if (error && error?.code === 409) {
-      const existingUser = await users.list([
-        Query.equal("email", [user.email]),
-      ]);
-
-      return existingUser.users[0];
-    }
-    console.error("An error occurred while creating a new user:", error);
-  }
-};
-/*
 export const createUser = async (user: CreateUserParams) => {
   try {
     console.log("Creating user with data:", user); // Log the input data
@@ -52,4 +27,3 @@ export const createUser = async (user: CreateUserParams) => {
     throw error; // Re-throw the error to handle it in the `onSubmit` function
   }
 };
-*/
